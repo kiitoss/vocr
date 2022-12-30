31147249cd96aee5f3f09fe7a4329c48a0b019a6
kiitoss <antoinechataignier@gmail.com>
2022-12-30 17:50:41 -0500

    feat: set up python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vocr',
+    version='1.0.0',
+    install_requires=['argparse', 'os', 'sys', 'json', 'easyocr', 'PIL', 'numpy', 'cv2', 'time', 'mss', 'screeninfo'],
+    description='Video Optical Character Recognition',
+    author='kiitoss',
+    maintainer='kiitoss',
+    license='MIT'
+)
