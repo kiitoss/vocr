e3b2ef40aca9911f9265408427c0fd5a0758b349
kiitoss <antoinechataignier@gmail.com>
2022-12-30 18:10:05 -0500

    fix: fix requires

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='vocr',
     version='1.0.0',
-    install_requires=['argparse', 'jsonlib', 'easyocr', 'Pillow', 'numpy', 'opencv-python', 'mss', 'screeninfo'],
+    install_requires=['argparse', 'jsonlib-python3', 'easyocr', 'Pillow', 'numpy', 'opencv-python', 'mss', 'screeninfo'],
     description='Video Optical Character Recognition',
     author='kiitoss',
     maintainer='kiitoss',
